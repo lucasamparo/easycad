997c4ac265854cc75268a3862619e4f3e9bff5ff
Lucas Amparo <lucasamparo.ti@gmail.com>
2015-09-12 13:20:27 -0300

    Criando POJO's. Script do BD implementado.

---
+++ easycad_schema.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 12-Set-2015 às 13:15
+-- Versão do servidor: 5.6.15-log
+-- PHP Version: 5.5.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `easycad`
+--
+CREATE DATABASE IF NOT EXISTS `easycad` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `easycad`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `certificado`
+--
+
+CREATE TABLE IF NOT EXISTS `certificado` (
+  `idCertificado` int(11) NOT NULL,
+  `idEntidade` int(11) DEFAULT NULL,
+  `codigo` varchar(12) DEFAULT NULL,
+  `dataEmissao` date DEFAULT NULL,
+  PRIMARY KEY (`idCertificado`),
+  KEY `idEntidade` (`idEntidade`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `curso`
+--
+
+CREATE TABLE IF NOT EXISTS `curso` (
+  `idCurso` int(11) NOT NULL,
+  `idEvento` int(11) DEFAULT NULL,
+  `nomeCurso` varchar(255) DEFAULT NULL,
+  `conteudo` text,
+  `valor` double DEFAULT NULL,
+  `cargaHoraria` int(11) DEFAULT NULL,
+  `dataCurso` date DEFAULT NULL,
+  PRIMARY KEY (`idCurso`),
+  KEY `idEvento` (`idEvento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empresa`
+--
+
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `idEmpresa` int(11) NOT NULL,
+  `nomeFantasia` varchar(255) DEFAULT NULL,
+  `razaoSocial` varchar(255) DEFAULT NULL,
+  `responsavel` varchar(255) DEFAULT NULL,
+  `login` varchar(30) DEFAULT NULL,
+  `senha` varchar(32) DEFAULT NULL,
+  `cnpj` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idEmpresa`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `entidade`
+--
+
+CREATE TABLE IF NOT EXISTS `entidade` (
+  `idEntidade` int(11) NOT NULL,
+  `nomeEntidade` varchar(255) DEFAULT NULL,
+  `cnpj_cpf` varchar(18) DEFAULT NULL,
+  `telefone` varchar(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `tipo` enum('PF','PJ') DEFAULT NULL,
+  PRIMARY KEY (`idEntidade`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `evento`
+--
+
+CREATE TABLE IF NOT EXISTS `evento` (
+  `idEvento` int(11) NOT NULL,
+  `nomeEvento` varchar(255) DEFAULT NULL,
+  `dataInicio` date DEFAULT NULL,
+  `dateFim` date DEFAULT NULL,
+  `modalidade` enum('P','O','PO','N') DEFAULT NULL,
+  `valor` double DEFAULT NULL,
+  `cargaHoraria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idEvento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `matricula`
+--
+
+CREATE TABLE IF NOT EXISTS `matricula` (
+  `idMatricula` int(11) NOT NULL,
+  `idEntidade` int(11) DEFAULT NULL,
+  `idCurso` int(11) DEFAULT NULL,
+  `dataHoraMatricula` datetime DEFAULT NULL,
+  PRIMARY KEY (`idMatricula`),
+  KEY `idEntidade` (`idEntidade`),
+  KEY `idCurso` (`idCurso`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `certificado`
+--
+ALTER TABLE `certificado`
+  ADD CONSTRAINT `certificado_ibfk_1` FOREIGN KEY (`idEntidade`) REFERENCES `entidade` (`idEntidade`);
+
+--
+-- Limitadores para a tabela `curso`
+--
+ALTER TABLE `curso`
+  ADD CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`idEvento`);
+
+--
+-- Limitadores para a tabela `matricula`
+--
+ALTER TABLE `matricula`
+  ADD CONSTRAINT `matricula_ibfk_2` FOREIGN KEY (`idCurso`) REFERENCES `curso` (`idCurso`),
+  ADD CONSTRAINT `matricula_ibfk_1` FOREIGN KEY (`idEntidade`) REFERENCES `entidade` (`idEntidade`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ models/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Verifique o caminho de acordo a versão baixada
+require '../libs/doctrine/Doctrine.php';
+
+spl_autoload_register(array('Doctrine', 'autoload'));
+spl_autoload_register(array('Doctrine_Core', 'modelsAutoload'));
+
+$manager = Doctrine_Manager::getInstance();
+try {
+  // Insira aqui os dados de sua conexão
+  $usuario = 'root';
+  $senha = '';
+  $local = 'localhost';
+  $database = 'easycad';
+  $conn = Doctrine_Manager::connection('mysql://'.$usuario.':'.$senha.'@'.$local.'/'.$database);
+
+  $manager->setAttribute(Doctrine_Core::ATTR_MODEL_LOADING,
+Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
+  $manager->setAttribute(Doctrine_Core::ATTR_EXPORT, Doctrine_Core::EXPORT_ALL);
+
+  $profiler = new Doctrine_Connection_Profiler();
+  $manager->setListener($profiler);
+
+} catch (Doctrine_Manager_Exception $e) {
+  print $e->getMessage();
+}
+
+Doctrine_Core::loadModels('../models');
+
+?>
