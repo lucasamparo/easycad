a9dd9c18cc3b981e77fa7092a7d51abcb43f9cbb
Breno Lessa <breno.lessa.andrade@gmail.com>
2015-09-12 22:42:42 -0300

    tela de login

--- app/index.php
+++ app/index.php
@@ -1,30 +1,51 @@
 <!DOCTYPE html>
 <html class="ls-theme-green">
-  <head>
-    <title>EasyCad</title>
+<head>
+  <meta charset="utf-8">
+  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <title>EasyCad</title>
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <link href="css/locastyle.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
 
-    <?php require_once('assets.php');?>
-    
-  </head>
-  <body>
+<div class="ls-login-parent">
+  <div class="ls-login-inner">
+    <div class="ls-login-container">
+      <div class="ls-login-box">
+  <h1 class="ls-login-logo"><img title="Logo login" src="images/logo-locaweb.jpg" /></h1>
+  <form role="form" class="ls-form ls-login-form" action="#">
+    <fieldset>
 
-    <?php require_once('header.php');?>
+      <label class="ls-label">
+        <b class="ls-label-text ls-hidden-accessible">Usuário</b>
+        <input class="ls-login-bg-user ls-field-lg" type="text" placeholder="Usuário" name="login" required autofocus>
+      </label>
 
-    <?php require_once('aside.php');?>
+      <label class="ls-label">
+        <b class="ls-label-text ls-hidden-accessible">Senha</b>
+        <div class="ls-prefix-group">
+          <input id="password_field" name="senha" class="ls-login-bg-password ls-field-lg" type="password" placeholder="Senha" required>
+          <a class="ls-label-text-prefix ls-toggle-pass ls-ico-eye" data-toggle-class="ls-ico-eye, ls-ico-eye-blocked" data-target="#password_field" href="#"></a>
+        </div>
+      </label>
 
-    <main class="ls-main ">
-      <div class="container-fluid">
-        <h1 class="ls-title-intro ls-ico-home">Página inicial</h1>
-        <!-- Conteúdo -->
-        
-        <p>Colocar os paranauês aqui!</p>
-        
-        
-        <!-- Fim Conteúdo -->
-      </div>
-    </main>
+      <input type="submit" value="Entrar" class="ls-btn-primary ls-btn-block ls-btn-lg">
 
-    <?php require_once('assets-footer.php');?>
+    </fieldset>
+  </form>
+</div>
 
-  </body>
+<div class="ls-login-adv"><img title="Empresa Júnior de Engenharia Ambiental" src="images/eambjr.png" width="296" height="127" /></div>
+
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script src="js/locastyle.js" type="text/javascript"></script>
+
+</body>
 </html>
