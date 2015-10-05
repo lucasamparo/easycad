5c7ac42aca40757fafbc183812d9fd4898638423
Breno Lessa <breno.lessa.andrade@gmail.com>
2015-10-04 23:32:20 -0300

    Configuração do Certificado ok

--- app/configuraCertificado.php
+++ app/configuraCertificado.php
@@ -121,6 +121,18 @@
 			});
 		});
     </script>
+
+    <style type="text/css">
+    	.contorno{
+    		/*border:1px dotted #CCC;*/
+    		padding: 30px;
+    	}
+    	.link{
+    		text-decoration: underline;
+    		color: #1AB551;
+    		text-transform: uppercase;
+    	}
+    </style>
   
   </head>
   <body>
@@ -131,30 +143,78 @@
 
     <main class="ls-main ">
       <div class="container-fluid">
-        <h1 class="ls-title-intro ls-ico-home">Configuração de Certificado</h1>
-        <h4>Certificado: <?= $titulo?></h4>
-        <form enctype="multipart/form-data" method="post" action="configuraCertificado.php?id=<?= $_GET['id']?>&t=<?= $_GET['t']?>">
-        	<label>Cor do Texto</label>
-        	<input type="color" name="cor" id="cor" value="<?= $corTexto?>"><input type="text" id="corsel" style="color: <?= $corTexto?>; font-weight: bold"width="20px" value="Cor da Fonte" readonly><br>
-        	<label>Layout do Certificado</label>
-        	<input type="radio" name="layout" value="1" id="l1" <?php if($layout == '1') echo 'checked'?>><label for="l1">Modelo 1</label>
-        	<input type="radio" name="layout" value="2" id="l2" <?php if($layout == '2') echo 'checked'?>><label for="l2">Modelo 2</label><br>
-        	<label>Possui verso?</label>
-        	<input type="radio" name="verso" value="S" id="versoS" <?php if($verso == 'S') echo 'checked'?>><label for="versoS">Sim</label>
-        	<input type="radio" name="verso" value="N" id="versoN" <?php if($verso == 'N') echo 'checked'?>><label for="versoN">Não</label><br>
-        	<label>Frente do Certificado (PNG):</label>
-        	<input type="file" name="frente" id="frente">
-        	<a href="imgCert/<?= $caminho_frente?>" target="_blank">Ver Atual</a><br>
-        	<?php 
-        		if($verso == 'S'){
-        			echo '<label>Fundo do Certificado (PNG):</label>';
-        			echo '<input type="file" name="fundo" id="fundo">';
-        			echo '<a href="imgCert/'.$caminho_fundo.'" target="_blank">Ver Atual</a><br>';
-        		}
-        	?>
-        	<input type="submit" value="Salvar Configurações">
-        </form>
-        <a href="listaCurso.php?id=<?= $idEvento?>">Voltar</a>
+
+        <h1 class="ls-title-intro ls-ico-home">Configuração do Certificado</h1>
+
+        <legend style="color:#1AB551; width:100%; font-size:20px; margin-bottom:20px;" class="ls-txt-center">Certificado: <?= utf8_encode($titulo)?></legend>
+
+        <div class="col-lg-12 col-xs-12">
+        	<form method="post" enctype="multipart/form-data" class="ls-form ls-form-horizontal row" action="configuraCertificado.php?id=<?= $_GET['id']?>&t=<?= $_GET['t']?>">
+
+        		<label class="ls-label col-lg-12 col-xs-12">
+	                <b class="ls-label-text">Cor do Texto:</b>
+	                <input type="color" name="cor" id="cor" value="<?= $corTexto?>" class="ls-field" style="height:35px;">
+	            </label>
+
+	            <label class="ls-label col-lg-12 col-xs-12">
+	                <b class="ls-label-text">Cor do Texto (Exemplo):</b>
+	                <input type="text" style="background-color:#e3e3e3;" id="corsel" style="color: <?= $corTexto?>; font-weight: bold" width="20px" class="ls-field" value="Cor do Texto" readonly>
+	            </label>
+
+
+	            <div class="ls-label col-lg-6 col-xs-12 contorno">
+	              <strong><p style="font-size:14px;">Layout do Certificado:</p></strong>
+
+	              <label class="ls-label-text">
+	                <input type="radio" class="ls-field-radio" name="layout" value="1" id="l1" <?php if($layout == '1') echo 'checked'?>>
+	                Modelo 1
+	              </label>
+
+	              <label class="ls-label-text">
+	                <input type="radio" class="ls-field-radio" name="layout" value="2" id="l2" <?php if($layout == '2') echo 'checked'?>>
+	                Modelo 2
+	              </label>
+
+	            </div>
+
+	            <div class="ls-label col-lg-6 col-xs-12 contorno">
+	              <strong><p style="font-size:14px;">Possui verso?</p></strong>
+
+	              <label class="ls-label-text">
+	                <input type="radio" class="ls-field-radio" name="verso" value="S" id="versoS" <?php if($verso == 'S') echo 'checked'?>>
+	                Sim
+	              </label>
+
+	              <label class="ls-label-text">
+	                <input type="radio" class="ls-field-radio" name="verso" value="N" id="versoN" <?php if($verso == 'N') echo 'checked'?>>
+	                Não
+	              </label>
+
+	            </div>
+
+	            <label class="ls-label col-lg-6 col-xs-12 contorno">
+	                <b class="ls-label-text">Frente do Certificado (PNG): | <a href="imgCert/<?= $caminho_frente?>" target="_blank" class="link">Ver Atual</a></b>
+	                <input type="file" name="frente" id="frente" class="ls-field">
+	                
+	            </label>
+
+
+	        	<?php 
+	        		if($verso == 'S'){
+	        			echo '<label class="ls-label col-lg-6 col-xs-12 contorno">';
+			                echo '<b class="ls-label-text">Fundo do Certificado (PNG): | <a href="imgCert/'.$caminho_fundo.'" target="_blank" class="link">Ver Atual</a></b>';
+			                echo '<input type="file" name="fundo" id="fundo" class="ls-field">';
+			                
+			            echo '</label>';
+	        		}
+	        	?>
+
+	        	<input type="submit" value= "Salvar Configurações" class="ls-btn-primary ls-btn-lg ls-text-uppercase col-lg-4 col-xs-11 col-lg-push-4" style="margin-bottom:50px;">
+        	</form>
+       	</div>
+
+
+        <a href="listaCurso.php?id=<?= $idEvento?>" class="ls-ico-circle-left ls-btn ls-btn-lg ls-text-uppercase col-lg-3 col-xs-11">Voltar</a>
       </div>
       <?php require_once('footer.php');?>
     </main>
