9782efec7329b15c8fa06081252c232e83745537
Lucas Amparo <lucasamparo.ti@gmail.com>
2016-01-14 11:24:48 -0300

    Correção no lançamento de presença.

--- app/lancaPresenca.php
+++ app/lancaPresenca.php
@@ -61,44 +61,42 @@
         <h1 class="ls-title-intro ls-ico-list2">Lançamento de Presença - Curso "<?= $curso->getNomeCurso()?>"</h1>
         <hr>
                 
-        <form method="post">
-        	<input type="hidden" name="valida" value="1">
-	        <table class="ls-table ls-bg-header ls-table-striped ls-table-bordered display" cellspacing="0" cellpadding="0" border="0">
-	        	<thead>
-	        		<th>Nome do Aluno</th>
-	        		<th>Presença</th>
-	        	</thead>
-	        	<tbody>
-	        		<?php 
-	        			//print_r($alunos->toArray());
-	        			foreach($alunos as $a){
-	        				foreach($presenca as $p){
-	        					if($a->getIdEntidade() == $p->getIdEntidade()){
-	        						echo '<tr>';
-		        						echo '<td>'.$a->getNomeEntidade().'</td>';
-		        						if($p->getPresenca() == 'P'){
-		        							echo '<td><input type="checkbox" name="p'.$a->getIdEntidade().'" id="p'.$a->getIdEntidade().'" checked></td>';
-		        						} else {
-		        							echo '<td><input type="checkbox" name="p'.$a->getIdEntidade().'" id="p'.$a->getIdEntidade().'"></td>';
-		        						}
-		        						
-	        						echo '</tr>';
-	        					}	
-	        				}
-	        			}
-	        		?>
-	        	</tbody>
-	        </table>
-
-	        <input type="submit" value="Lançar Presença" class="ls-btn-primary ls-btn-lg ls-text-uppercase col-lg-4 col-xs-11 col-lg-push-4">
-        </form>
-
-        <div class="ls-label col-lg-12 col-xs-12"></div>
-
-        <div>
+        <div class="ls-label col-lg-12 col-xs-12">
+        	<form method="post">
+	        	<input type="hidden" name="valida" value="1">
+		        <table class="ls-table ls-bg-header ls-table-striped ls-table-bordered display" cellspacing="0" cellpadding="0" border="0">
+		        	<thead>
+		        		<th>Nome do Aluno</th>
+		        		<th>Presença</th>
+		        	</thead>
+		        	<tbody>
+		        		<?php 
+		        			//print_r($alunos->toArray());
+		        			foreach($alunos as $a){
+		        				foreach($presenca as $p){
+		        					if($a->getIdEntidade() == $p->getIdEntidade()){
+		        						echo '<tr>';
+			        						echo '<td>'.$a->getNomeEntidade().'</td>';
+			        						if($p->getPresenca() == 'P'){
+			        							echo '<td><input type="checkbox" name="p'.$a->getIdEntidade().'" id="p'.$a->getIdEntidade().'" checked></td>';
+			        						} else {
+			        							echo '<td><input type="checkbox" name="p'.$a->getIdEntidade().'" id="p'.$a->getIdEntidade().'"></td>';
+			        						}
+			        						
+		        						echo '</tr>';
+		        					}	
+		        				}
+		        			}
+		        		?>
+		        	</tbody>
+		        </table>
+	
+		        <input type="submit" value="Lançar Presença" class="ls-btn-primary ls-btn-lg ls-text-uppercase col-lg-4 col-xs-11 col-lg-push-4">
+	        </form>
+        </div>
+        <div class="ls-label col-lg-12 col-xs-12">
         	<a href="listaCurso.php?id=<?= $curso->getEvento()->getIdEvento()?>" class="ls-ico-circle-left ls-btn ls-btn-lg ls-text-uppercase col-lg-3 col-xs-11">Voltar</a>
         </div>
-		
       </div>
       <?php require_once('footer.php');?>
     </main>
